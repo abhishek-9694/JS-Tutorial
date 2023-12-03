a5189a93e9a3eb663b32c62049a7d79101ecd3cb
Abhishek Singh <111600443+abhishek-9694@users.noreply.github.com>
2023-12-03 12:34:27 +0000

    container config done

---
+++ 01_basics/start.js
@@ -0,0 +1,8 @@
+console.log("Abhishek singh");
+ 
+console.log("Abhishek Singh");
+let a = 10;
+let b = 20;
+let c = a*b;
+console.log("THE Multiplication of the Two Variable:",c);
+// document.write("THE Multiplication of the Two Variable:",a,"*",b,"=",c);
